ef29e3ac4d640982f098038b2bec44ad521f3c4a
michele <michele.provolo@gmail.com>
2019-11-19 23:57:22 +0100

    commenti

--- main.js
+++ main.js
@@ -9,37 +9,38 @@
 
 $(document).ready(function () {
 
-
+	
 	$.ajax({
 		url: "https://flynn.boolean.careers/exercises/api/array/music",
 		method: "GET",
 		success: function (data) {
 			// console.log("success", data.response);
-
+			
 			//questo ciclo for mi restituisce ogni singolo oggetto
-			for (var i = 0; i < data.response.length; i++) {
+			for(var i = 0; i < data.response.length; i++) {
 				console.log(data.response[i]);
+			
+			// source restituisce il contenuto dello script di handlebars
+			var source = $(".record").text();
+			// console.log(source);
+			var template = Handlebars.compile(source);
+			
+			//salvo in context un oggetto che ha come chiave gli elementi del mio template HB
+			//e come valore i relativi dati dell'API
+			var context = 
+			{
+			poster: data.response[i].poster,
+			title: data.response[i].title,
+			author: data.response[i].author,
+			genre: data.response[i].genre,
+			year: data.response[i].year
+			};
+			var html = template(context);
+			// console.log(html);
 
-				// source restituisce il contenuto dello script di handlebars
-				var source = $(".record").text();
-				// console.log(source);
-				var template = Handlebars.compile(source);
-
-				//salvo in context un oggetto che ha come chiave gli elementi del mio template HB
-				//e come valore i relativi dati dell'API
-				var context = {
-					img: data.response[i].poster,
-					title: data.response[i].title,
-					author: data.response[i].author,
-					genre: data.response[i].genre,
-					year: data.response[i].year
-				};
-				var html = template(context);
-				// console.log(html);
-
-				//appendo in un contenitore html qualunque il contenuto dell'oggetto suddetto
-				$(".cds-container").append(html);
-			}
+			//appendo in un contenitore html qualunque il contenuto dell'oggetto suddetto
+			$(".cds-container").append(html);
+		}
 		},
 		error: function (stato) {
 			console.log("c'è stato un errore: " + stato);
