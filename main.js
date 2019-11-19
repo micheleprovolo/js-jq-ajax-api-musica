04212c636ceae7f27ca3543241095e5ea8644d1f
michele <michele.provolo@gmail.com>
2019-11-19 15:45:54 +0100

    init 1

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
-// DESCRIZIONE:
-//  Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali.
-// Servendoci di handlebars stampiamo tutto a schermo. In questo momento non è importante la parte grafica.
-// Chiamata:
-// https://flynn.boolean.careers/exercises/api/array/music
-// Layout base (per chi non vuole implementarne uno suo):
-// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
-
-$(document).ready(function (){
-
- 
+$(document).ready(function() {
+	//Code
 });
