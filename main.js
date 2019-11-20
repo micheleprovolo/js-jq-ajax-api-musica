3c1112fdda7f41213770a2e82f733a425cf41f65
michele <michele.provolo@gmail.com>
2019-11-20 15:19:01 +0100

    altri commenti

--- main.js
+++ main.js
@@ -9,38 +9,47 @@
 
 $(document).ready(function () {
 
-	
+
 	$.ajax({
 		url: "https://flynn.boolean.careers/exercises/api/array/music",
 		method: "GET",
 		success: function (data) {
 			// console.log("success", data.response);
-			
-			//questo ciclo for mi restituisce ogni singolo oggetto
-			for(var i = 0; i < data.response.length; i++) {
-				console.log(data.response[i]);
-			
-			// source restituisce il contenuto dello script di handlebars
-			var source = $(".record").text();
-			// console.log(source);
-			var template = Handlebars.compile(source);
-			
-			//salvo in context un oggetto che ha come chiave gli elementi del mio template HB
-			//e come valore i relativi dati dell'API
-			var context = 
-			{
-			poster: data.response[i].poster,
-			title: data.response[i].title,
-			author: data.response[i].author,
-			genre: data.response[i].genre,
-			year: data.response[i].year
-			};
-			var html = template(context);
-			// console.log(html);
+			//capisco che il response è un array di oggetti
 
-			//appendo in un contenitore html qualunque il contenuto dell'oggetto suddetto
-			$(".cds-container").append(html);
-		}
+
+			//questo ciclo for mi restituisce ogni singolo oggetto dell'array
+			for (var i = 0; i < data.response.length; i++) {
+				// come esempio per capire prendo in considerazione il valore della proprietà title dell'oggetto dentro l'array
+				console.log(data.response[i].title);
+
+				//con ciclo for in sui singoli oggetti che stanno ciclando tramite il ciclo for
+				var oggetto = data.response[i]
+				var x;
+				for (x in oggetto) {
+					console.log(x + " : " + oggetto[x]);
+				}
+
+				// source restituisce il contenuto dello script di handlebars
+				var source = $(".record").text();
+				// console.log(source);
+				var template = Handlebars.compile(source);
+
+				//salvo in context un oggetto che ha come chiave gli elementi del mio template HB
+				//e come valore i relativi dati dell'API
+				var context = {
+					poster: data.response[i].poster,
+					title: data.response[i].title,
+					author: data.response[i].author,
+					genre: data.response[i].genre,
+					year: data.response[i].year
+				};
+				var html = template(context);
+				// console.log(html);
+
+				//appendo in un contenitore html qualunque il contenuto dell'oggetto suddetto
+				$(".cds-container").append(html);
+			}
 		},
 		error: function (stato) {
 			console.log("c'è stato un errore: " + stato);
